f1841b05877e7a9bd18a73dd4090bf72b105dc67
cloud-370 <13484677105@163.com>
2017-04-21 09:39:33 +0800

    the last version of python3-webapp copy from liaoxuefeng

--- www/models.py
+++ www/models.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Models for user, blog, comment.
+'''
+
+__author__ = 'Michael Liao'
+
 import time, uuid
 
 from orm import Model, StringField, BooleanField, FloatField, TextField
 
 def next_id():
-    return '%015d%s000' % (int(time.time()*1000), uuid.uuid4().hex)
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
 class User(Model):
     __table__ = 'users'
@@ -14,7 +23,7 @@ class User(Model):
     admin = BooleanField()
     name = StringField(ddl='varchar(50)')
     image = StringField(ddl='varchar(500)')
-    create_at = FloatField(default=time.time)
+    created_at = FloatField(default=time.time)
 
 class Blog(Model):
     __table__ = 'blogs'
